3d54e7ca2f476d6671b06c1bfee5db98eab433b9
Sergio Neves <sernevess@gmail.com>
2022-04-12 22:40:16 +0100

    Remove sneaky .env

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,1 @@
 export const proxyUrl = "https://simple-proxy.vercel.app/api/proxy?url=";
-
-export const proxyToken =
-  "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzZWVuZXZ6In0.odmtKkaMxE28J10Gi5NOLmUHNpzmrn2Gi1JU2qKuUBWeXY4ww5ToDse4GAEVc2DqfBAcHDE5PBhDig-s_7V0dg";
