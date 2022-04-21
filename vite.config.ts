d2ce50632d93fbd9ccd3861e0bb5e847fb8588ae
Sergio Neves <sernevess@gmail.com>
2022-04-21 23:38:47 +0100

    Add css modules

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import preact from "@preact/preset-vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  css: {
+    modules: {
+      localsConvention: "camelCaseOnly"
+    }
+  }
   // server: {
   //   hmr: {
   //     port: 443,
