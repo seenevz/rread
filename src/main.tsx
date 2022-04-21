309ff537e603fd954f43f7e90c311b87bba3c0d0
Sergio Neves <sernevess@gmail.com>
2022-04-21 23:39:38 +0100

    Root attach refactoring

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { render } from "preact";
 import { App } from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { app } from "./styles/base.module.css";
+
+const div = document.createElement("div");
+div.id = app;
+document.body.prepend(div);
 
 render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById("app")!
+  div
 );
