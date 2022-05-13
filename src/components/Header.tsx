cba5e570775162077e796377bd4ae09db77e0ad7
Sergio Neves <sernevess@gmail.com>
2022-05-13 18:34:17 +0100

    Add side menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,42 @@
+import { useState } from "preact/hooks";
 import rssIcon from "../assets/icons/rss-square-solid.svg";
-import { navbar, hamburger } from "../styles/header.module.css";
+import {
+  navbar,
+  hamburger,
+  aside,
+  asideOpen,
+} from "../styles/header.module.css";
 
 export default function Header() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
     <nav class={navbar} role="navigation" aria-label="main navigation">
       <a>
         <img src={rssIcon} style={"width: 2em"} />
       </a>
-      <a
-        role="button"
+      <button
         class={hamburger}
         aria-label="menu"
-        aria-expanded="false">
+        aria-expanded="false"
+        onClick={() => setSidebarOpen(val => !val)}>
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
-      </a>
+      </button>
+      <aside class={`${aside} ${sidebarOpen && asideOpen}`}>
+        <ul>
+          <li>
+            <a href="">One</a>
+          </li>
+          <li>
+            <a href="">Two</a>
+          </li>
+          <li>
+            <a href="">Three</a>
+          </li>
+        </ul>
+      </aside>
     </nav>
   );
 }
