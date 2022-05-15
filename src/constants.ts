2cf0d54112f10e7bca25a7e4dac993e764cf801a
Sergio Neves <sernevess@gmail.com>
2022-05-15 01:54:22 +0100

    Add missing constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
 export const proxyUrl = "https://simple-proxy.vercel.app/api/proxy?url=";
 
-export const proxyToken = import.meta.env.VITE_TOKEN
+
+export const proxyToken = import.meta.env.VITE_TOKEN || "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzZWVuZXZ6In0.kQvp3okPugaL6aoP5nG5wkVMxi_NjF6OkpeE9pVTQriHkLtNAaGHX__N_M6sA4JrCei9VBasWtFn3OCdnwFftg"
