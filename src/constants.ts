522e1a84e53efa21ea9cceaa1e7be51e07042b1b
Sergio Neves <sernevess@gmail.com>
2022-05-22 00:39:52 +0100

    Changes to endpoints

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
 export const proxyUrl = "https://simple-proxy.vercel.app/api/proxy?url=";
+// export const proxyUrl = "http://192.168.1.129:3001/api/proxy?url="
 
 
-export const proxyToken = import.meta.env.VITE_TOKEN || "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzZWVuZXZ6In0.kQvp3okPugaL6aoP5nG5wkVMxi_NjF6OkpeE9pVTQriHkLtNAaGHX__N_M6sA4JrCei9VBasWtFn3OCdnwFftg"
+export const proxyToken = import.meta.env.VITE_TOKEN ?? ""
