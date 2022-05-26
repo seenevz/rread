66825403ac7409b45c1d19cd26fabb047c46cffb
Sergio Neves <sernevess@gmail.com>
2022-05-26 22:40:58 +0100

    Integrate add feed component

--- src/components/AddFeed.tsx
+++ src/components/AddFeed.tsx
@@ -1,13 +1,22 @@
+import { useState } from "preact/hooks";
 import styles from "../styles/home.module.css";
 
 const { addFeed, addForm } = styles;
 
 export default function AddFeed() {
+  const [url, setUrl] = useState("");
+
   return (
     <main className={addFeed}>
       <h2>Add a feed url here</h2>
       <form className={addForm}>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={e => setUrl(e.target.value)}
+          value={url}
+          placeholder="https://somefeed.com/rss"
+        />
+        {url.length ? <input type="submit" /> : null}
       </form>
     </main>
   );
